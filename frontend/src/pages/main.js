33b054383743608e4c8ecace42c80ad307c272ad
luongphucdien <work.luongphucdien@gmail.com>
2022-05-24 18:19:40 +0700

    Dien | Modify | Generate bar chart

--- frontend/src/pages/main.js
+++ frontend/src/pages/main.js
@@ -2,13 +2,13 @@ import React, { useEffect, useState } from 'react';
 import * as d3 from 'd3'
 import { nest } from 'd3-collection';
 
-const w=600, h=420, padding=50;
+const w=600, h=420, padding=50, range = 10;
 var dataset;
 
 export default function Main() {
     useEffect(() => {
         d3.select(".svg").append("svg")
-            .attr("class", "line-chart")
+            .attr("class", "bar-chart")
             .attr("width", w+100)
             .attr("height", h+padding*2);
 
@@ -24,35 +24,54 @@ export default function Main() {
         };
 
         d3.csv('/data/archive/Age-standardized-suicide-rates.csv', rowConverter).then((data) => {
-            const nestedData = nest()
+            const data_2016_both = nest()
                 .key((d) => d.country)
-                .rollup((d) => {
-                    return ({
-                        bothSex: {  '2016': d[0].year2016, 
-                                    '2015': d[0].year2015,
-                                    '2010': d[0].year2010,
-                                    '2000': d[0].year2000   },
-
-                        male: { '2016': d[1].year2016, 
-                                '2015': d[1].year2015,
-                                '2010': d[1].year2010,
-                                '2000': d[1].year2000   },
-
-                        female: {   '2016': d[2].year2016, 
-                                    '2015': d[2].year2015,
-                                    '2010': d[2].year2010,
-                                    '2000': d[2].year2000   },
-
-                        all: d
-                    });
-                })
+                .rollup((d) => { return d[0].year2016; })
                 .entries(data);
             
-            dataset = nestedData;
+            dataset = data_2016_both;
         });
         
         setTimeout(() => {
-            console.log(dataset);
+            console.table(dataset);
+
+            const barChart = d3.select('.bar-chart');
+            const y_max_2016 = d3.max(dataset, (d) => {
+                return d.value;
+            });
+            console.log(y_max_2016);
+
+            var xScale = d3.scaleBand()
+                            .domain(d3.range(dataset.length))
+                            .range([padding, w+padding])
+                            .paddingInner(0.05);
+
+            var yScale = d3.scaleLinear()
+                            .domain([0, y_max_2016+1])
+                            .range([h, 0])
+                            .nice()
+
+            var key = (d) => {
+                return d.key;
+            }
+
+            barChart.selectAll("rect")
+                    .data(dataset, key)
+                    .enter()
+                    .append("rect")
+                    .attr("y", (d) => {
+                        return yScale(d.value);
+                    })
+                    .attr("x", function(d, i) {
+                        return xScale(i);
+                    })
+                    .attr("height", function(d) {
+                        return h - yScale(d.value);
+                    })
+                    .attr("width", function(d) {
+                        return xScale.bandwidth();
+                    })
+                    .attr("fill", 'blue');
         }, 200);
     }, []);
 
